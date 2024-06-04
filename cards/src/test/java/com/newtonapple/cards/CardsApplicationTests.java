48b6a009a8fa7b4890c6aa41badb845037e40930
HP <HP@host.docker.internal>
2024-06-04 22:56:15 +0530

    Tests for Cards and Loans

--- cards/src/test/java/com/newtonapple/cards/CardsApplicationTests.java
+++ cards/src/test/java/com/newtonapple/cards/CardsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.newtonapple.accounts;
+package com.newtonapple.cards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccountsApplicationTests {
+class CardsApplicationTests {
 
 	@Test
 	void contextLoads() {
