b39b4db915847cda480dac0c0a1b030be29e6923
HP <HP@host.docker.internal>
2024-06-04 22:51:12 +0530

    Cards Dto Commit

--- cards/src/main/java/com/newtonapple/cards/dto/CardsDto.java
+++ cards/src/main/java/com/newtonapple/cards/dto/CardsDto.java
@@ -2,4 +2,11 @@ package com.newtonapple.cards.dto;
 
 public class CardsDto {
 
+	
+	private String mobileNumber;
+	private String cardNumber;
+	private String cardType;
+	private int totalLimit;
+	private String amountUsed;
+	private String availableAmount;
 }
